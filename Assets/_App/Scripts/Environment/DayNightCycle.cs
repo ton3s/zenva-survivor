97a47980964a16305a8540fea5f8b9eec720d2dc
Antonio Cunanan <antonio.cunanan@gmail.com>
2023-12-27 16:21:12 -0600

    Day/Night cycle

---
+++ Assets/_App/Scripts/Environment/DayNightCycle.cs
@@ -0,0 +1,80 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DayNightCycle : MonoBehaviour
+{
+	[Range(0.0f, 1.0f)]
+	public float time;
+	public float fullDayLength;
+	public float startTime = 0.4f;
+	private float timeRate;
+	public Vector3 noon;
+
+	[Header("Sun")]
+	public Light sun;
+	public Gradient sunColor;
+	public AnimationCurve sunIntensity;
+
+	[Header("Moon")]
+	public Light moon;
+	public Gradient moonColor;
+	public AnimationCurve moonIntensity;
+
+	[Header("Other Lighting")]
+	public AnimationCurve lightIntensityMultiplier;
+	public AnimationCurve reflectionsIntensityMultiplier;
+
+	void Start()
+	{
+		timeRate = 1.0f / fullDayLength;
+		time = startTime;
+	}
+
+	void Update()
+	{
+		// Increment time
+		time += timeRate * Time.deltaTime;
+
+		if (time >= 1.0f)
+		{
+			time = 0.0f;
+		}
+
+		// Light rotation
+		sun.transform.eulerAngles = (time - 0.25f) * noon * 4.0f;
+		moon.transform.eulerAngles = (time - 0.75f) * noon * 4.0f;
+
+		// Light intensity
+		sun.intensity = sunIntensity.Evaluate(time);
+		moon.intensity = moonIntensity.Evaluate(time);
+
+		// Change colors
+		sun.color = sunColor.Evaluate(time);
+		moon.color = moonColor.Evaluate(time);
+
+		// Enable / disable the sun
+		if (sun.intensity == 0 && sun.gameObject.activeInHierarchy)
+		{
+			sun.gameObject.SetActive(false);
+		}
+		else if (sun.intensity > 0 && !sun.gameObject.activeInHierarchy)
+		{
+			sun.gameObject.SetActive(true);
+		}
+
+		// Enable / disable the moon
+		if (moon.intensity == 0 && moon.gameObject.activeInHierarchy)
+		{
+			moon.gameObject.SetActive(false);
+		}
+		else if (moon.intensity > 0 && !moon.gameObject.activeInHierarchy)
+		{
+			moon.gameObject.SetActive(true);
+		}
+
+		// Lighting and reflections intensity
+		RenderSettings.ambientIntensity = lightIntensityMultiplier.Evaluate(time);
+		RenderSettings.reflectionIntensity = reflectionsIntensityMultiplier.Evaluate(time);
+	}
+}
