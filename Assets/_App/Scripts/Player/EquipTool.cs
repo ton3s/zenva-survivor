2f15c56b77419abedd4551e5f7f7fd8ee330068f
Antonio Cunanan <antonio.cunanan@gmail.com>
2024-01-03 13:32:17 -0600

    Equip tool script

--- Assets/_App/Scripts/Player/EquipTool.cs
+++ Assets/_App/Scripts/Player/EquipTool.cs
@@ -4,15 +4,50 @@ using UnityEngine;
 
 public class EquipTool : Equip
 {
-	// Start is called before the first frame update
-	void Start()
+	public float attackRate;
+	private bool attacking;
+	public float attackDistance;
+
+	[Header("Resource Gathering")]
+	public bool doesGatherResources;
+
+	[Header("Combat")]
+	public bool doesDealDamage;
+	public int damage;
+
+	// Components
+	private Animator anim;
+	private Camera cam;
+
+	void Awake()
+	{
+		// Get components
+		anim = GetComponent<Animator>();
+		cam = Camera.main;
+	}
+
+	public override void OnAttackInput()
+	{
+		if (!attacking)
+		{
+			attacking = true;
+			anim.SetTrigger("Attack");
+			Invoke("OnCanAttack", attackRate);
+		}
+	}
+
+	void OnCanAttack()
+	{
+		attacking = true;
+	}
+
+	public void OnHit()
 	{
 
 	}
 
-	// Update is called once per frame
-	void Update()
+	public override void OnAltAttackInput()
 	{
-
+		base.OnAltAttackInput();
 	}
 }
